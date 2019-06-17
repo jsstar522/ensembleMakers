51d375feab8826dbe09430e4cf747dace1754ff2
pseudotop <lsupertopl@gmail.com>
2019-06-17 23:17:35 +0000

    Setup proxy server and static build method

---
+++ client/src/setupProxy.js
@@ -0,0 +1,18 @@
+const proxy = require("http-proxy-middleware");
+
+module.exports = function(app) {
+	app.use(
+		proxy("/api", {
+			//target: "http://172.32.2.61:5000/",
+			target: "http://Ensemble-NLB-b125ba92bb371a53.elb.ap-northeast-2.amazonaws.com:5000",
+			changeOrigin: true
+		})
+	);
+	app.use(
+		proxy("/auth", {
+			//target: "http://172.32.2.61:5000/",
+			target: "http://Ensemble-NLB-b125ba92bb371a53.elb.ap-northeast-2.amazonaws.com:5000",
+			changeOrigin: true
+		})
+	);
+};
