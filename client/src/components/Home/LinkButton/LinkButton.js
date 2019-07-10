95260801e160bb670b13aab66a35acce9d902864
JongSuk <jsstar522@gmail.com>
2019-07-10 17:19:31 +0900

    Front-end files

--- client/src/components/Home/LinkButton/LinkButton.js
+++ client/src/components/Home/LinkButton/LinkButton.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './LinkButton.scss';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const BorderedButton = styled(Link)`
+  display: table-cell;
+  vertical-align: middle;
+  border: 1px solid red;
+  border-radius: 5px;
+  width: 40%;
+  cursor: pointer;
+`;
+
 
 class LinkButton extends Component {
   render() {
     const { logged, userNumber } = this.props;
     return(
       <div className="link-button-wrapper">
-        <a className="link-button" href={logged ? "/orderManage":"/auth/login"}>주문관리</a>
-        <a className="link-button" href={logged ? "/customerInfo/"+userNumber:"/auth/login"}>주문받기</a>
+        <BorderedButton to={logged?"/orderManage":"/auth/login"}>주문관리</BorderedButton>
+        <BorderedButton to={logged?"/customerInfo/"+userNumber:"/auth/login"}>주문받기</BorderedButton>
       </div>
     )
   }
